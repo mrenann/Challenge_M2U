aed904c266331de4bb83fd0a29c3b8c7b018463f
Marcos Renann <marcos.renann.br@gmail.com>
2021-03-11 12:23:40 -0300

    Create card_movie.xml

---
+++ app/src/main/java/com/mrenann/challengem2u/view/activity/MovieDetailedActivity.kt
@@ -0,0 +1,12 @@
+package com.mrenann.challengem2u.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mrenann.challengem2u.R
+
+class MovieDetailedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detailed)
+    }
+}
